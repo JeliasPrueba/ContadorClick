1d7c08acc53b74c0551ff9ace9a5c687fa7418f7
Jelias <garciassierra@hotmail.com>
2024-04-03 20:05:49 -0400

    Funcionamiento del contador

--- contadorapp/src/App.js
+++ contadorapp/src/App.js
@@ -1,16 +1,33 @@
 
 import './App.css';
 
+import {useState} from "react"
+
 function App() {
+
+  const [numero, setNumero] = useState(0);
+
+  const Contador = () =>{
+    setNumero(numero + 1);
+  }
+
+  const reset = () =>{
+    setNumero(0);
+  }
+
   return (
+    
     <div className="Contenedor">
+      
         <div className="Contador_Btn">
           <div className="Contador">
-            <h1 className="Numero">5</h1>
+          <h1>Contador de Clicks</h1>
+            <h1 className="Numero">{numero}</h1>
           </div>
           
           <div className="Btn">
-            <button>Click</button>
+            <button onClick={Contador}>Click</button>
+            <button onClick={reset}>Reset</button>
           </div>
         </div>
 
